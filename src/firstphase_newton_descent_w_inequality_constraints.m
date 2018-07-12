22bdd47188ef2c0399ce447561b9a5c410ea1b4f
Sridhar Thiagarajan <thiagarajan14111@mech.ssn.edu.in>
2018-07-13 04:31:17 +0530

    Update firstphase_newton_descent_w_inequality_constraints.m

--- src/firstphase_newton_descent_w_inequality_constraints.m
+++ src/firstphase_newton_descent_w_inequality_constraints.m
@@ -1,4 +1,4 @@
-function [x,feasibility] = phase1_newton_descent_w_inequality_constraints(f,C,x0_feasible, finite_diff_eps, stopping_eps, alpha, beta)
+function [x,feasibility] = firstphase_newton_descent_w_inequality_constraints(f,C,x0_feasible, finite_diff_eps, stopping_eps, alpha, beta)
 
 % min s
 % s.t. Cx = d
